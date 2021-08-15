e7e35daa4f5aa68d78772e21dbfce2fbade857e8
William Chau <wchau@oraculo.nl>
2021-08-15 14:42:09 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+class Antwoord {
+  eigenschap() {
+    return "hello";
+  }
+};
+
+class Klant {
+  naam = "Google";
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  antwoord1: any ;
+  antwoord2: any = new Antwoord();
+  klant = new Klant();
+
   title = 'AngularFundamentals-Binding';
+  interpolationVariabele = "interpolation";
+
+
+  parameter = "TypeScript";
+
+  tweeWeg:any = "bla";
+
+  clickButton(param?:string):void {
+    if (param)
+      alert('hello '+param+'!');
+    else
+      alert('hello from Angular!');
+  }
+
+  telOp(getal:number):number {
+    return getal+100;
+  }
+
+  onItemClick(event:Event)
+  {
+    console.log("Event: ",event);
+  }
+
 }
