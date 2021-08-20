9810e8318c9b5c751df443ce72ce8e84f684dd66
William Chau <wchau@oraculo.nl>
2021-08-20 17:12:04 +0000

    rekenmachine WIP: input OK, still need result + optional input restriction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RekenmachineComponent } from './rekenmachine/rekenmachine.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'rekenmachine', component: RekenmachineComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RekenmachineComponent } from './rekenmachine/rekenmachine.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RekenmachineComponent
   ],
   imports: [
     BrowserModule,
