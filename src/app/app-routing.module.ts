5ee827caefeb816819718cde770901c889dc1c4c
oraculo-nl <info@oraculo.nl>
2021-09-09 15:07:05 +0200

    - remove rekenmachinecomponent - add opdracht rekenmachine instructions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RekenmachineComponent } from './rekenmachine/rekenmachine.component';
 
-const routes: Routes = [{path:'rekenmachine', component: RekenmachineComponent}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RekenmachineComponent } from './rekenmachine/rekenmachine.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RekenmachineComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
